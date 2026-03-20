317ea592aae8d905d6e5640bb09cfb9f9b983439
Atorresp7 <atorresp2@unicartagena.edu.co>
2026-03-20 18:03:56 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/clinicalhospital/Medico.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.clinicalhospital;
+
+/**
+ *
+ * @author Andres Felipe Torres Padilla
+ */
+public class Medico extends Persona{
+    private String especialidad;
+    private String tarjetaProfesional;
+
+    public Medico() {
+    }
+
+    public Medico(String especialidad, String tarjetaProfesional, String nombre, int edad, String cedula) {
+        super(nombre, edad, cedula);
+        this.especialidad = especialidad;
+        this.tarjetaProfesional = tarjetaProfesional;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getTarjetaProfesional() {
+        return tarjetaProfesional;
+    }
+
+    public void setTarjetaProfesional(String tarjetaProfesional) {
+        this.tarjetaProfesional = tarjetaProfesional;
+    }
+
+    @Override
+    public String toString() {
+        return "Medico{" + "especialidad=" + especialidad + ", tarjetaProfesional=" + tarjetaProfesional + '}';
+    }
+               
+}
---
+++ src/main/java/com/mycompany/clinicalhospital/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.clinicalhospital;
+
+/**
+ *
+ * @author Andres Felipe Torres Padilla
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String cedula;
+    
+    public Persona(){}
+    
+    public Persona(String nombre, int edad, String cedula){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cedula = cedula;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    
+    public String getCedula(){
+        return cedula;
+    }
+    
+    public void setCedula(String cedula){
+        this.cedula = cedula;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\tPersona \nNombre=" + nombre + "\nEdad=" + edad + "\nCedula=" + cedula;
+    }
+    
+    
+}
